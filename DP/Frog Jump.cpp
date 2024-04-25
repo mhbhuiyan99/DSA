5f0b115510c499947d8dcac0f8392ff3d9a87cf8
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2024-04-25 16:16:26 +0600

    Update Frog Jump.cpp

--- DP/Frog Jump.cpp
+++ DP/Frog Jump.cpp
@@ -1,5 +1,7 @@
 Problem Link : https://bit.ly/3JPcoOx
 
+>>>  Top-Down approach  
+
 int solve(int n, vector<int> &heights, vector<int> &dp)
 {
     if(n == 0) return 0;
@@ -17,4 +19,22 @@ int frogJump(int n, vector<int> &heights)
 {
     vector<int> dp(n+1, -1);
     return solve(n-1, heights, dp);
+}
+
+--------------------------------------------------------------------------------
+>>>  Tabulation Approach
+
+int frogJump(int n, vector<int> &heights)
+{
+    vector<int> dp(n);
+    dp[0] = 0;
+    for(int i=1; i < n; i++)
+    {
+        int oneMove = dp[i-1] + abs( heights[i] - heights[i-1] );
+        int twoMove = INT_MAX;
+        if(i > 1)
+            twoMove = dp[i-2] + abs( heights[i] - heights[i-2] );
+        dp[i] = min(oneMove, twoMove);
+    }
+    return dp[n-1];
 }
