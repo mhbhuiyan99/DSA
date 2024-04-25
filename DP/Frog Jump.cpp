bad54d581adc7b304f489c0f6d69346a5fef17c4
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2024-04-25 15:45:14 +0600

    codingningas

---
+++ DP/Frog Jump.cpp
@@ -0,0 +1,20 @@
+Problem Link : https://bit.ly/3JPcoOx
+
+int solve(int n, vector<int> &heights, vector<int> &dp)
+{
+    if(n == 0) return 0;
+    if(dp[n] != -1) return dp[n];
+
+    int oneMove = solve(n - 1, heights, dp) + abs(heights[n] - heights[n-1]);
+    int twoMove = INT_MAX;
+    if(n > 1) 
+        twoMove = solve(n-2, heights, dp) + abs(heights[n] - heights[n-2]);
+
+    return dp[n] = min(oneMove, twoMove);
+}
+
+int frogJump(int n, vector<int> &heights)
+{
+    vector<int> dp(n+1, -1);
+    return solve(n-1, heights, dp);
+}
