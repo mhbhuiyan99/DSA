202bb4cc5294cc2b473d1d9c2a29bfba5f5f4bc7
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2024-05-03 22:21:49 +0600

    Update 6. Unique Paths.cpp

--- DP/6. Unique Paths.cpp
+++ DP/6. Unique Paths.cpp
@@ -2,14 +2,14 @@ Problem Link : https://leetcode.com/problems/unique-paths/description/
 
 >>>  Memorization Approach
 
-    int solve(int m, int n, vector<vector<int>> &dp)
+int solve(int m, int n, vector<vector<int>> &dp)
     {
-        if(dp[m][n] != -1)
-            return dp[m][n];
         if(m == 0 and n == 0)
             return 1;
         if(m < 0 or n < 0)
             return 0;
+        if(dp[m][n] != -1)
+            return dp[m][n];
 
         int left = solve(m-1,n,dp);
         int up = solve(m, n-1,dp);
