19f5d7c1f9c65c21fc3e6c8ab6c01f0c2dd102c2
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2024-05-16 14:49:52 +0600

    Le

--- DP/8. Minimum Path Sum.cpp
+++ DP/8. Minimum Path Sum.cpp
@@ -21,3 +21,51 @@ int solve(int i, int j, vector<vector<int>>&grid, vector<vector<int>>&dp)
     }
 -------------------------------------------------------------------------
 >>>  Tabulation Approach
+
+int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int> (m, 0));
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(i == 0 and j == 0) dp[i][j] = grid[i][j];
+                else
+                {
+                    int up = INT_MAX, left = INT_MAX;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
+                    dp[i][j] = grid[i][j] + min(up, left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+
+>>>    Space Optimization Approach
+
+int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<int> pre(m);
+        
+        for(int i=0; i<n; i++)
+        {
+            vector<int> tmp(m);
+            for(int j=0; j<m; j++)
+            {
+                if(i == 0 and j == 0) tmp[j] = grid[i][j];
+                else
+                {
+                    int up = INT_MAX, left = INT_MAX;
+                    if(i > 0) up = pre[j];
+                    if(j > 0) left = tmp[j-1];
+                    tmp[j] = grid[i][j] + min(up, left);
+                }
+            }
+            pre = tmp;
+        }
+        return pre[m-1];
+    }
