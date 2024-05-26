6ca6904a554b450e92b5b8d21dff120b15908bbd
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2024-05-26 10:18:31 +0600

    LeetCode

--- DP/10. Minimum Falling Path Sum.cpp
+++ DP/10. Minimum Falling Path Sum.cpp
@@ -24,3 +24,29 @@ int minFallingPathSum(vector<vector<int>>& matrix) {
         return ans;
     }
 ------------------------------------------------------------------------------
+
+>>>    Tabulation Approach
+
+int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int> (n, 0));
+
+        for(int i=0; i<n; i++)
+            dp[0][i] = matrix[0][i];
+
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                int cost = dp[i-1][j];
+                if(j > 0) cost = min(cost, dp[i-1][j-1]);
+                if(j + 1 < n) cost = min(cost, dp[i-1][j+1]);
+                dp[i][j] = cost + matrix[i][j];
+            }
+        }
+        int ans = dp[n-1][0];
+        for(int i=1; i<n; i++)
+            ans = min(ans, dp[n-1][i]);
+
+        return ans;
+    }
