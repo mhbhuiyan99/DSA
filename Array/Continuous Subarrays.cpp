8c9c9a4b47b3fa312a7585ebef1f1bbbda6060ff
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2024-12-14 21:21:11 +0600

    common pattern

---
+++ Array/Continuous Subarrays.cpp
@@ -0,0 +1,3 @@
+Problem Link: [2762. Continuous Subarrays](https://leetcode.com/problems/continuous-subarrays/description/)
+
+[4 approach solution](https://leetcode.com/problems/continuous-subarrays/solutions/6140505/continuous-subarrays)
