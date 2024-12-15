85029a02eed5efa4df249e9e1cd074f2027802c9
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2024-12-15 22:14:34 +0600

    Priority Queue

---
+++ Queue/Maximum Average Pass Ratio.cpp
@@ -0,0 +1,44 @@
+Problem Link : https://leetcode.com/problems/maximum-average-pass-ratio/description/
+
+double maxAverageRatio(vector<vector<int>>& classes, int eS) {
+        
+        priority_queue< pair<double, pair<int,int> > > pq;
+
+        int n = classes.size();
+
+        for(int i = 0; i<n; i++)
+        {
+            int pass = classes[i][0];
+            int total = classes[i][1];
+            
+            double dif = (double)(pass + 1)/(total + 1) - (double)pass/total;
+
+            pq.push({dif, {pass, total}});
+        }
+
+        while(eS--)
+        {
+            int pass = pq.top().second.first;
+            int total = pq.top().second.second;
+            pass++;
+            total++;
+
+            double newDif = (double)(pass + 1)/(total + 1) - (double)pass/total;
+
+            pq.pop();
+            pq.push({newDif, {pass, total}});
+        }
+
+        double ratio = 0;
+        while(!pq.empty()) 
+        {
+            int pass = pq.top().second.first;
+            int total = pq.top().second.second;
+
+            ratio += (double)(pass)/(total);
+
+            pq.pop();
+        }
+
+        return ratio / n;
+    }
