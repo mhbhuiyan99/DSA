160b4802b8c0bc6f34d62e97c1ae51fd6fd842bd
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2025-01-06 21:39:23 +0600

    Easy BUT Optimizing***

--- Array/Minimum Number of Operations to Move All Balls to Each Box.cpp
+++ Array/Minimum Number of Operations to Move All Balls to Each Box.cpp
@@ -1,1 +1,25 @@
 Problem Link : https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/
+
+>>> ---- Optimized - O(n) ---- <<<
+
+vector<int> minOperations(string boxes) {
+        
+        int n = boxes.size();
+        vector<int> ans(n,0);
+
+        for(int i=0, ops = 0, cnt = 0; i<n; i++)
+        {
+            ans[i] += ops;
+            cnt += (boxes[i] - '0');
+            ops += cnt;
+        }
+
+        for(int i=n-1, ops = 0, cnt = 0; i>=0; i--)
+        {
+            ans[i] += ops;
+            cnt += (boxes[i] - '0');
+            ops += cnt;
+        }
+
+        return ans;
+    }
